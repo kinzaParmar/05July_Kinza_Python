43c5fee0c61f9bcf6f65bee48e05dd385aa0020b
kinzaParmar <109806224+kinzaParmar@users.noreply.github.com>
2022-08-12 20:03:26 +0530

    Delete count_2_more.py

--- Module 3/count_2_more.py
+++
@@ -1,7 +0,0 @@
-#Program to count the number of strings, where the lenght of string is 2 or more and first n last char are same 
-str_list=['I','Kinza','Python','eat','is']
-for i in str_list:
-    str=len(i)<=2 and i[0] == i[:-1]
-    str_count=str.count(i)
-print(str_count)
-
